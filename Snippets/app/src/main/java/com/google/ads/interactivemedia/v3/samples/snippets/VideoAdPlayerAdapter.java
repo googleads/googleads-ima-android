cb9b9e2fba9ddbfc032a549cbc6d58b13d132c2a
Jackson Sui <jsui@google.com>
2025-10-10 12:01:03 -0700

    Adds a Snippets sample app for IMA documentation code snippets.
    Includes a snippet for the Enable Open Measurement guide.
    
    PiperOrigin-RevId: 817737078

---
+++ Snippets/app/src/main/java/com/google/ads/interactivemedia/v3/samples/snippets/VideoAdPlayerAdapter.java
@@ -0,0 +1,59 @@
+// Copyright 2025 Google LLC
+
+package com.google.ads.interactivemedia.v3.samples.snippets;
+
+import android.media.AudioManager;
+import android.widget.VideoView;
+import com.google.ads.interactivemedia.v3.api.AdPodInfo;
+import com.google.ads.interactivemedia.v3.api.player.AdMediaInfo;
+import com.google.ads.interactivemedia.v3.api.player.VideoAdPlayer;
+import com.google.ads.interactivemedia.v3.api.player.VideoProgressUpdate;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Example implementation of IMA's VideoAdPlayer interface. */
+public class VideoAdPlayerAdapter implements VideoAdPlayer {
+
+  private final VideoView videoPlayer;
+  private final AudioManager audioManager;
+  private final List<VideoAdPlayerCallback> videoAdPlayerCallbacks = new ArrayList<>();
+
+  public VideoAdPlayerAdapter(VideoView videoPlayer, AudioManager audioManager) {
+    this.videoPlayer = videoPlayer;
+    this.audioManager = audioManager;
+  }
+
+  @Override
+  public void addCallback(VideoAdPlayerCallback videoAdPlayerCallback) {
+    videoAdPlayerCallbacks.add(videoAdPlayerCallback);
+  }
+
+  @Override
+  public void loadAd(AdMediaInfo adMediaInfo, AdPodInfo adPodInfo) {}
+
+  @Override
+  public void pauseAd(AdMediaInfo adMediaInfo) {}
+
+  @Override
+  public void playAd(AdMediaInfo adMediaInfo) {}
+
+  @Override
+  public void release() {}
+
+  @Override
+  public void removeCallback(VideoAdPlayerCallback videoAdPlayerCallback) {}
+
+  @Override
+  public void stopAd(AdMediaInfo adMediaInfo) {}
+
+  @Override
+  public int getVolume() {
+    return 0;
+  }
+
+  @Override
+  public VideoProgressUpdate getAdProgress() {
+    long adPosition = videoPlayer.getCurrentPosition();
+    return new VideoProgressUpdate(adPosition, 0);
+  }
+}
